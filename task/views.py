dd5a89f67d8044805fb34ba68a2b426ec81e9f53
tahseenRaj <raj.tahseen@gmail.com>
2021-07-27 19:20:24 +0500

    django overview

---
+++ task/views.py
@@ -0,0 +1,35 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+from django import forms
+
+
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+
+# Create your views here.
+def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
+    return render(request, "task/index.html", {
+        "tasks": request.session["tasks"]
+    })
+
+
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("task:index"))
+        else:
+            return render(request, "task/add.html", {
+                "form": form
+            })
+
+    return render(request, "task/add.html", {
+        "form": NewTaskForm()
+    })
